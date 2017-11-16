fa3971f708268e3d6bd5035c109101028857a31c
charlyBraga <charlybraga@gmail.com>
2017-11-16 14:29:53 -0200

    Mauricio

---
+++ src/main/Algorithm.kt
@@ -0,0 +1,11 @@
+package main
+
+/**
+ * Created by Aluno on 16/11/2017.
+ */
+class Algorithm {
+    val descripition : String
+    val title : String
+    val author : String
+    val version : String
+}
