4fafb6b883f82641f98341a205b542d449c19c24
charlyBraga <charlybraga@gmail.com>
2017-11-16 14:35:50 -0200

    Mauricio

---
+++ src/main/NumericAlgorithm.kt
@@ -0,0 +1,7 @@
+package main
+
+/**
+ * Created by Aluno on 16/11/2017.
+ */
+class NumericAlgorithm {
+}
---
+++ src/main/StringAlgorithm.kt
@@ -0,0 +1,17 @@
+package main
+
+/**
+ * Created by Aluno on 16/11/2017.
+ */
+class StringAlgorithm {
+    fun creatPattern () {
+        val PatternCypher = arrayOF("ABCDEFGHIJKLMNOPRSTUVXYZ")
+    }
+    fun cesarCypher(inputText:String, patternCypher:CharArray)
+    {
+
+    }
+    fun main(args: Array<String>){
+        printl("My Project")
+    }
+}
